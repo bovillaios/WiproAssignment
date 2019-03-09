63bd7471eea09bfc1f582c8a3c43236ed468f9ff
Ramgopal <>
2019-03-09 15:32:21 +0530

    Project Setup

---
+++ WiproAssignment/WiproAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiproAssignment
+//
+//  Created by GGKU5MACBOOK003 on 09/03/19.
+//  Copyright © 2019 Ramgopal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
